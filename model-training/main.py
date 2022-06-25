9b70e37c20e937c347c6627ae1d6bbdec2d07410
Other <other@f018988206ed.ant.amazon.com>
2022-06-25 19:23:39 -0300

    initial commit

---
+++ model-training/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+from joblib import dump, load
+
+raw_training_data = pd.read_csv("./dataset/training.csv", on_bad_lines='skip', sep=';')
+raw_test_data = pd.read_csv("./dataset/test.csv", on_bad_lines='skip', sep=';')
+
+train_tweets = raw_training_data[["text"]]
+train_is_tweet_american = raw_training_data[["country_code"]].apply(lambda x: x == "US").astype(int)
+
+test_tweets = list(raw_training_data[["text"]].values.ravel())
+test_is_tweet_american = list(raw_training_data[["country_code"]].apply(lambda x: x == "US").astype(int).values.ravel())
+
+tweet_clf = Pipeline([
+     ('vect', CountVectorizer()),
+     ('tfidf', TfidfTransformer()),
+     ('clf', MultinomialNB()),
+])
+
+tweet_clf.fit(train_tweets.values.ravel(), train_is_tweet_american.values.ravel())
+
+
+dump(tweet_clf, 'american_tweet_recognizer_model.joblib')
+clf = load('american_tweet_recognizer_model.joblib')
+
+predicted = clf.predict(test_tweets)
+
+print(np.mean(predicted == test_is_tweet_american))
+print("test")
